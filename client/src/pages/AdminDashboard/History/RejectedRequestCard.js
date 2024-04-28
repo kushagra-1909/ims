5d7a505b130f59f250d90a28e320398bdc92ceef
kushagra-1909 <kushagraface123@gmail.com>
2024-04-29 01:21:04 +0530

    rejected request css

--- client/src/pages/AdminDashboard/History/RejectedRequestCard.js
+++ client/src/pages/AdminDashboard/History/RejectedRequestCard.js
@@ -7,23 +7,52 @@ const RejectedRequestCard = ({ request }) => {
   const { createdBy, dateRequested, Items, message } = request;
   return (
     <div className="rejected-request-card">
-      <h3>Faculty: {createdBy.username}</h3>
-      <p>Date: {new Date(dateRequested).toLocaleDateString()}</p>
-      <ul>
-        {Items.map((item, index) => (
-          <li key={index}>
-            <span>Item Name: {item.item.itemName} </span>
-            <span>Quantity Requested: {item.quantityRequested}</span>
-          </li>
-        ))}
-      </ul>
-      <p>Reason : {message}</p>
-      <PDFDownloadLink
-        document={<PDFDocument Items={Items} />}
-        fileName="userRequestDetail"
-      >
-        <button className="download-pdf">Download PDF</button>
-      </PDFDownloadLink>
+      <div>
+        <h3> {createdBy.username}</h3>
+        <h5>{createdBy.designation}</h5>
+      </div>
+      <div>
+        <p>Date: {new Date(dateRequested).toLocaleDateString()}</p>
+        <p>Time : {new Date(dateRequested).toLocaleTimeString()}</p>
+      </div>
+      <div className="rejected-request-card-items-content">
+        <div className="rejected-request-card-item-header">
+          <div>
+            <span>Item Name</span>
+          </div>
+          <div>
+            <span>Quantity Requested</span>
+          </div>
+          <div>
+            <span>Quantity Approved</span>
+          </div>
+        </div>
+        <div className="rejected-request-items-content">
+          <ul>
+            {Items.map((item, index) => (
+              <li key={index}>
+                <div>
+                  <span>{item.item.itemName} </span>
+                </div>
+                <div>
+                  <span>{item.quantityRequested}</span>
+                </div>
+                <div>
+                  <span>{item.quantityApproved}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="rejected-request-card-action-container">
+        <PDFDownloadLink
+          document={<PDFDocument Items={Items} />}
+          fileName="userRequestDetail"
+        >
+          <button className="download-pdf">Download PDF</button>
+        </PDFDownloadLink>
+      </div>
     </div>
   );
 };
